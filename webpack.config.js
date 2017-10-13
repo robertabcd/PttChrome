d7a4e2d56e2344bed5abcf684e21834d71b9a710
Robert <robert@arctic.tw>
2017-10-12 20:38:32 -0700

    Pack other assets.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-const appName = 'pttchrome';
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: [
-    './js/main.js'
-  ],
+  entry: {
+    'pttchrome': [
+      './js/main.js',
+      './css/main.css',
+      './css/color.css'
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: appName + '.min.js'
+    filename: '[name].min.js'
   },
   resolve: {
     extensions: ['.js', '.jsx']
@@ -23,13 +26,34 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: {
+            loader: "css-loader",
+            options: {
+              minimize: true,
+              sourceMap: true
+            }
+          }
+        })
+      },
+      {
+        test: /\.(bmp|png|woff)$/,
+        loader: "file-loader"
       }
     ]
   },
   devtool: 'source-map',
   plugins: [
     new UglifyJSPlugin({
-      sourceMap: true
+      sourceMap: true,
+      parallel: true
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].min.css'
     })
   ]
 };
