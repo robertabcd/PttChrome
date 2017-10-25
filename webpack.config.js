668848aeb441e70cdaeac5244478d232f32f3a4f
Tom Chen <developer@tomchentw.com>
2017-10-25 22:27:15 +0800

    chore(package.json): move babel config

--- webpack.config.js
+++ webpack.config.js
@@ -32,10 +32,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ['es2015', 'react']
-        }
+        loader: "babel-loader"
       },
       {
         test: /\.css$/,
