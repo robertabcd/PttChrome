dc7e9e3260f72b709015991f26f49d64936010f1
Tom Chen <developer@tomchentw.com>
2017-11-24 16:23:19 +0800

    refactor(components/Row): create LinkSegmentBuilder with ColorSegmentBuilder

--- src/js/term_ui.jsx
+++ src/js/term_ui.jsx
@@ -1,6 +1,4 @@
-import { HyperLinkPreview } from './image_preview';
-import HyperLink from '../components/HyperLink';
-import Text from '../components/Text';
+import Row from '../components/Row';
 
 export class ColorState {
   constructor(fg, bg, blink) {
@@ -14,80 +12,6 @@ export class ColorState {
       return this.fg == oth.fg && this.bg == oth.bg && this.blink == oth.blink;
     }
     return false;
-  }
-}
-
-class Row extends React.Component {
-  constructor() {
-    super();
-    this.state = {highlighted: false};
-  }
-
-  _segmentHyperLinks(chars) {
-    let segments = [];
-    let cur = null;
-    for (let i = 0; i < chars.length; i++) {
-      let ch = chars[i];
-      if (!cur || ch.isStartOfURL()) {
-        cur = {
-          col: i,
-          href: ch.isStartOfURL() ? ch.getFullURL() : null,
-          chars: []
-        };
-        segments.push(cur);
-      }
-      cur.chars.push(ch);
-      if (ch.isEndOfURL())
-        cur = null;
-    }
-    return segments;
-  }
-
-
-  render() {
-    let cols = [];
-    let linkPreviews = [];
-    for (let linkSeg of this._segmentHyperLinks(this.props.chars)) {
-      let inner = [
-        <Text
-          key={`text-${cols.length}`}
-          chars={linkSeg.chars}
-          forceWidth={this.props.forceWidth}
-        />
-      ]
-
-      if (linkSeg.href) {
-        let key = 'hyperLink-c-' + linkSeg.col;
-        cols.push(<HyperLink key={key} href={linkSeg.href} inner={inner}
-          data-scol={linkSeg.col} data-srow={this.props.row} />);
-        // TODO: Modularize this.
-        if (this.props.showsLinkPreviews) {
-          linkPreviews.push(
-            <HyperLinkPreview key={key} src={linkSeg.href} />);
-        }
-      } else {
-        cols = cols.concat(inner);
-      }
-    }
-    let classes = [];
-    if (this.state.highlighted) {
-      classes.push('b2');
-    }
-    // TODO: Detect userid and apply class "blu_$userid".
-    return (
-      <div>
-        <span key="line" className={classes.join(' ')}
-            data-type="bbsline"
-            data-row={this.props.row}>{cols}</span>
-        <div key="previews">{linkPreviews}</div>
-      </div>
-    );
-  }
-
-  setHighlight(shouldHighlight) {
-    if (this.state.highlighted != shouldHighlight) {
-      this.setState({highlighted: shouldHighlight});
-    }
   }
 }
 
