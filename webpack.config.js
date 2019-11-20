475e9c00fe88fae3f368f55702f72f9455a20004
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-11-21 00:36:09 +0800

    refactor(webpack.config.js): refine production mode handling for `webpack@4`
    - package.json & webpack.config.js: Refine handling of production mode variables.
    - webpack.config.js: Refine configuration category.
    - Eliminate a dependency: `cross-env`.

--- webpack.config.js
+++ webpack.config.js
@@ -6,126 +6,130 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const WebpackCdnPlugin = require('webpack-cdn-plugin');
 
-const DEVELOPER_MODE = process.env.NODE_ENV === 'development'
-const PRODUCTION_MODE = process.env.NODE_ENV === 'production'
+module.exports = (env, argv) => {
+  const DEVELOPER_MODE = argv.mode === 'development';
+  const PRODUCTION_MODE = argv.mode === 'production';
 
-module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: {
-    'pttchrome': './src/entry.js',
-  },
-  output: {
-    path: path.join(__dirname, 'dist/assets/'),
-    publicPath: 'assets/',
-    pathinfo: DEVELOPER_MODE,
-    filename: `[name]${ PRODUCTION_MODE ? '.[chunkhash]' : '' }.js`
-  },
-  externals: {
-    jquery: 'jQuery',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: './'
-            }
-          },
-          {
-            loader: "css-loader",
-            options: {
-              minimize: PRODUCTION_MODE,
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(bin|bmp|png|woff)$/,
-        oneOf: [
-          {
-            resourceQuery: /inline/,
-            use: 'url-loader'
-          },
-          {
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[hash].[ext]'
-                }
+  return {
+    entry: {
+      'pttchrome': './src/entry.js',
+    },
+    output: {
+      path: path.join(__dirname, 'dist/assets/'),
+      publicPath: 'assets/',
+      filename: `[name]${ PRODUCTION_MODE ? '.[chunkhash]' : '' }.js`
+    },
+    externals: {
+      jquery: 'jQuery',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: './'
               }
-            ]
+            },
+            {
+              loader: "css-loader",
+              options: {
+                minimize: PRODUCTION_MODE,
+                sourceMap: true
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(bin|bmp|png|woff)$/,
+          oneOf: [
+            {
+              resourceQuery: /inline/,
+              use: 'url-loader'
+            },
+            {
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[hash].[ext]'
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    devtool: 'source-map',
+    optimization: {
+      minimizer: [
+        new UglifyJSPlugin({
+          sourceMap: true,
+          parallel: true
+        }),
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.PTTCHROME_PAGE_TITLE': JSON.stringify(process.env.PTTCHROME_PAGE_TITLE || 'PttChrome'),
+        'process.env.DEFAULT_SITE': JSON.stringify(PRODUCTION_MODE ? 'wsstelnet://ws.ptt.cc/bbs' : 'wstelnet://localhost:8080/bbs'),
+        'process.env.ALLOW_SITE_IN_QUERY': JSON.stringify(process.env.ALLOW_SITE_IN_QUERY === 'yes'),
+        'process.env.DEVELOPER_MODE': JSON.stringify(DEVELOPER_MODE),
+      }),
+      new MiniCssExtractPlugin({
+        disable: DEVELOPER_MODE,
+        filename: '[name].[chunkhash].css'
+      }),
+      new HtmlWebpackPlugin({
+        alwaysWriteToDisk: DEVELOPER_MODE,
+        minify: {
+          collapseWhitespace: PRODUCTION_MODE,
+          removeComments: PRODUCTION_MODE
+        },
+        inject: 'head',
+        template: './src/dev.html',
+        filename: '../index.html'
+      }),
+      new WebpackCdnPlugin({
+        modules: [
+          {
+            name: 'react',
+            var: 'React',
+            path: 'umd/react.production.min.js',
+          },
+          {
+            name: 'react-dom',
+            var: 'ReactDOM',
+            path: 'umd/react-dom.production.min.js',
+          },
+        ],
+      }),
+      ... DEVELOPER_MODE ? [
+        new HtmlWebpackHarddiskPlugin()
+      ] : []
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, './dist'),
+      proxy: {
+        '/bbs': {
+          target: 'https://ws.ptt.cc',
+          secure: true,
+          ws: true,
+          changeOrigin: true,
+          onProxyReqWs(proxyReq) {
+            // Whitelist does not accept ws.ptt.cc
+            proxyReq.setHeader('origin', 'https://term.ptt.cc');
           }
-        ]
-      }
-    ]
-  },
-  devtool: 'source-map',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.PTTCHROME_PAGE_TITLE': JSON.stringify(process.env.PTTCHROME_PAGE_TITLE || 'PttChrome'),
-      'process.env.DEFAULT_SITE': JSON.stringify(PRODUCTION_MODE ? 'wsstelnet://ws.ptt.cc/bbs' : 'wstelnet://localhost:8080/bbs'),
-      'process.env.ALLOW_SITE_IN_QUERY': JSON.stringify(process.env.ALLOW_SITE_IN_QUERY === 'yes'),
-      'process.env.DEVELOPER_MODE': JSON.stringify(DEVELOPER_MODE),
-    }),
-    new MiniCssExtractPlugin({
-      disable: DEVELOPER_MODE,
-      filename: '[name].[chunkhash].css'
-    }),
-    new HtmlWebpackPlugin({
-      alwaysWriteToDisk: DEVELOPER_MODE,
-      minify: {
-        collapseWhitespace: PRODUCTION_MODE,
-        removeComments: PRODUCTION_MODE
-      },
-      inject: 'head',
-      template: './src/dev.html',
-      filename: '../index.html'
-    }),
-    new WebpackCdnPlugin({
-      modules: [
-        {
-          name: 'react',
-          var: 'React',
-          path: 'umd/react.production.min.js',
-        },
-        {
-          name: 'react-dom',
-          var: 'ReactDOM',
-          path: 'umd/react-dom.production.min.js',
-        },
-      ],
-    })
-  ].concat(PRODUCTION_MODE ? [
-    new UglifyJSPlugin({
-      sourceMap: true,
-      parallel: true
-    }),
-  ] : [
-    new HtmlWebpackHarddiskPlugin()
-  ]),
-  devServer: {
-    contentBase: path.join(__dirname, './dist'),
-    proxy: {
-      '/bbs': {
-        target: 'https://ws.ptt.cc',
-        secure: true,
-        ws: true,
-        changeOrigin: true,
-        onProxyReqWs(proxyReq) {
-          // Whitelist does not accept ws.ptt.cc
-          proxyReq.setHeader('origin', 'https://term.ptt.cc');
         }
       }
     }
-  }
+  };
 };
