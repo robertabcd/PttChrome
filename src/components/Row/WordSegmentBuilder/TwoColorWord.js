4f940b0e30e446bad7e9ded4c1ad4ff22b5fea7b
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2021-05-13 23:40:24 +0800

    feat(TwoColorWord.js): implement blinking
    TwoColorWord achieves the two-color-word effect
    by adding a half-word overlay to a full-width character ("word").
    
    By making use of the overlay,
    the blinking effect can be achieved by the following cases:
    * Only the tail blinks: Overlay the head and blink only the underneath word
    * Only the head blinks: Overlay the tail and blink only the underneath word
    * Both blink: Overlay the head and blink both the underneath word and the overlay
    
    For overlaying the tail,
    a negative text-indent is applied to the overlay
    to truncate off the left half of the overlay character.
    
    The background color is handled by the .b* CSS classes
    but was also affected by the .qq* classes.
    This further complicated the TwoColorWord blinking implementation.
    
    Therefore, I removed the .qq* classes,
    except for the .qq class, which does not affect the background color.
    
    * src/components/Row/WordSegmentBuilder/ColorSpan.js
        * Use CSS class .qq for the blinking effect
    * src/components/Row/WordSegmentBuilder/TwoColorWord.js
        * Add implementation description
        * Implement blinking
    * src/css/color.css
        * Use class .qq for the blinking effect solely and remove other .qq* classes

--- src/components/Row/WordSegmentBuilder/TwoColorWord.js
+++ src/components/Row/WordSegmentBuilder/TwoColorWord.js
@@ -2,17 +2,48 @@ import cx from "classnames";
 import { forceWidthStyle } from "./ForceWidthWord";
 
 /**
- * TODO: add blinking.
+ * TwoColorWord implements the two-color-word effect,
+ * where the first half ("head") and the last half ("tail")
+ * of a fullwidth character ("word") have different colors.
+ *
+ * The two-color-word effect is achieved by adding a half-word overlay to a word,
+ * so that the overlaid half of the word has the color of the overlay.
+ *
+ * If both the head and the tail have the same foreground color and blinking effect,
+ * the half-word overlay is not needed.
+ *
+ * The blinking effect is achieved by the following cases:
+ * - Only the tail blinks: Overlay the head and blink only the underneath word
+ * - Only the head blinks: Overlay the tail and blink only the underneath word
+ * - Both blink: Overlay the head and blink both the underneath word and the overlay
  */
 export const TwoColorWord = ({ colorLead, colorTail, forceWidth, text }) => (
   <span
     className={cx({
-      [`q${colorLead.fg}`]: colorLead.fg === colorTail.fg,
-      [`w${colorLead.fg}`]: colorLead.fg !== colorTail.fg,
-      [`q${colorTail.fg}`]: colorLead.fg !== colorTail.fg,
-      o: colorLead.fg !== colorTail.fg,
+      // classes for foreground
+      ...(colorLead.fg === colorTail.fg
+        ? {
+            // for one-color-word (including blinking differences)
+            [`q${colorLead.fg}`]: true,
+            [`w${colorTail.fg}`]: colorLead.blink !== colorTail.blink
+          }
+        : {
+            // for two-color-word with only the head blinks
+            [`q${colorLead.fg}`]: colorLead.blink && !colorTail.blink,
+            [`w${colorTail.fg}`]: colorLead.blink && !colorTail.blink,
+            // for two-color-word in other cases
+            [`w${colorLead.fg}`]: !colorLead.blink || colorTail.blink,
+            [`q${colorTail.fg}`]: !colorLead.blink || colorTail.blink
+          }),
+      // classes for the half-word overlay
+      o: colorLead.fg !== colorTail.fg || colorLead.blink !== colorTail.blink,
+      ot: colorLead.blink && !colorTail.blink, // overlay the tail instead
+      // classes for background
       [`b${colorLead.bg}`]: colorLead.bg === colorTail.bg,
       [`b${colorLead.bg}b${colorTail.bg}`]: colorLead.bg !== colorTail.bg,
+      // other classes
+      qq: colorLead.blink || colorTail.blink, // blink the underneath word
+      ww: colorLead.blink && colorTail.blink, // blink the half-word overlay
       wpadding: forceWidth
     })}
     style={forceWidthStyle(forceWidth)}
